4e925af3237e3442f84c25d1ebbed949b78443ac
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-02-19 10:28:54 +0800

    Adds preseeds to be deployed on install

--- manifest/init.pp
+++ manifest/init.pp
@@ -4,11 +4,68 @@ class ccgsimplecobbler () inherits ccgsimplecobbler::params {
         ensure  => absent,
     }
 
-    file {'/opt/cobbler/systemsetup.py':
-         content => template('/systemsetup.py.erb'),
-    }
-
     package { $ccgsimplecobbler::packages:
          ensure  => present
+    }
+
+
+    file {'/opt/cobbler/systemsetup.py':
+         content => template('/systemsetup.py'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+
+    file {'/etc/cobbler/dhcp.template':
+         content => template('/cobbler/dhcp.template'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+
+    # preseeds
+    file {'/etc/cobbler/ccgcloud.preseed':
+         content => template('/cobbler/ccgcloud.preseed'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+    file {'/etc/cobbler/ccgswift.preseed':
+         content => template('/cobbler/ccgswift.preseed'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+    file {'/etc/cobbler/ceph.preseed':
+         content => template('/cobbler/ceph.preseed'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+    file {'/etc/cobbler/compute.preseed':
+         content => template('/cobbler/compute.preseed'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+    file {'/etc/cobbler/controller.preseed':
+         content => template('/cobbler/controller.preseed'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
+    }
+    file {'/etc/cobbler/ubuntu-server.preseed':
+         content => template('/cobbler/ubuntu-server.preseed'),
+         ensure => present,
+         owner => 'root',
+         group => 'root',
+         mode => 0644,
     }
 }
