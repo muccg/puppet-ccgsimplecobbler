6f526921a60beede0b9c8a29a9ad93862491bf4e
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-05-02 17:04:04 +0800

    knock down some crack houses in the slum

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,84 +9,83 @@ class ccgsimplecobbler () inherits ccgsimplecobbler::params {
         ensure  => present
     }
 
-
-    file { "/ccgcobbler":
-        ensure => "directory",
-        owner  => "root",
-        group  => "root",
-        mode   => 750,
+    file { '/ccgcobbler':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
     } ->
 
     file {'/ccgcobbler/systemsetup.py':
+        ensure  => present,
         content => template('ccgsimplecobbler/systemsetup.py.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/dhcp.template':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/dhcp.template.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/zone.template':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/zone.template.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     # preseeds
     file {'/etc/cobbler/ccgcloud.preseed.erb':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/ccgcloud.preseed.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/ccgswift.preseed':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/ccgswift.preseed.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/ceph.preseed':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/ceph.preseed.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/compute.preseed':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/compute.preseed.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/controller.preseed':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/controller.preseed.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 
     file {'/etc/cobbler/ubuntu-server.preseed':
+        ensure  => present,
         content => template('ccgsimplecobbler/cobbler/ubuntu-server.preseed.erb'),
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 }
